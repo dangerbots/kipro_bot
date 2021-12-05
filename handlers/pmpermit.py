58601a681e2fe68c1846b33bc1756e873e1b099d
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2021-12-05 12:29:31 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"𝗧𝗵𝗶𝘀 𝗜𝘀 𝗕𝗼𝘁 𝗔𝘀𝘀𝗶𝘀𝘁𝗮𝗻𝗰𝗲 𝗝𝗼𝗶𝗻 @danger_bots 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽")
+  await USER.send_message(message.chat.id,"this id bot assistant 🤐 contact @dangerpro_bot for help👻")
   return                        
