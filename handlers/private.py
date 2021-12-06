e14816b04b866a6fe3b33cf87f3ef7432e18e0f1
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2021-12-06 10:49:53 +0530

    Create private.py

---
+++ handlers/private.py
@@ -0,0 +1,48 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+from helpers.filters import other_filters2
+
+
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+    await message.reply_sticker("CAACAgEAAxkBAAECL0phG6f50Xd7OG\_i8YaNIzz7gh2UbAAC2gEAAuaC4EQGx1I9UzNAziAE")
+    await message.reply_text(
+        f"""**Hey, I'm {bn} 🎀
+ɪ ᴄᴀɴ ᴘʟᴀʏ ᴍᴜsɪᴄ ɪɴ ʏᴏᴜʀ  ɢʀᴏᴜᴩ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ. 
+ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴩ ᴀɴᴅ ᴘʟᴀʏ ᴍᴜsɪᴄ ғʀᴇᴇʟʏ 🤗 Developed By [👻👻](https://t.me/Kidqueen_bot) !**
+        """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🔨Source Code🔨", url="https://t.me/Kidqueen_bot")
+                  ],[
+                    InlineKeyboardButton(
+                        "sᴜᴘᴘᴏʀᴛ❤", url="https://t.me/Status_world2"
+                    ),
+                    InlineKeyboardButton(
+                        "DEV", url="https://t.me/danger_of_telegram"
+                    )    
+                ],[ 
+                    InlineKeyboardButton(
+                        "➕ᴀᴅᴅ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ➕", url="https://t.me/kidqueen?startgroup=true"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text("""**Yes iᴍ online ✅**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🔊Uᴩᴅᴀᴛᴇs", url="https://t.me/danger_of_telegram")
+                ]
+            ]
+        )
+   )
